2ab530966c5817429cabc0a03ace676f66f547cb
FanJekkk <denteplyakov2020@gmail.com>
2019-05-06 16:18:07 +1000

    top

---
+++ include/db_connect.php
@@ -0,0 +1,12 @@
+<?php
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpassword = '';
+$dbname = 'example';
+
+$link = mysqli_connect($dbhost, $dbuser, $dbpassword);
+
+mysqli_select_db($link,$dbname);
+
+
+?>
